eec2fcdaf92aa2c078965ac977bd36389b5016e3
mookyongkim <dbkorea@nate.com>
2024-07-19 16:28:20 +0900

    Update Page_1.py

--- {{cookiecutter.project_slug}}/pages/Page_1.py
+++ {{cookiecutter.project_slug}}/pages/Page_1.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 current_page = "Page 1"
 st.title("Page 1")
-navbar.nav("Page 1")
+navbar.nav(current_page)
 st.write("This is a dummy page for {{ cookiecutter.project_slug }}.")
